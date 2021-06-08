76d7ad0d4a377bbf591bc97ca0f364b2905a86fb
Sergey Shpakov <guttleslug@mail.ru>
2021-06-09 01:18:22 +0300

    Добавьте файлы проекта.

---
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -0,0 +1,71 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "Components/STUHealthComponent.h"
+#include "GameFramework//Actor.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All);
+
+// Sets default values for this component's properties
+USTUHealthComponent::USTUHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+
+// Called when the game starts
+void USTUHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+    check(MaxHealth > 0);
+
+    SetHealth(MaxHealth);
+    OnHealthChanged.Broadcast(Health);
+
+	AActor* ComponentOwner = GetOwner();
+    if (ComponentOwner)
+    {
+        ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &USTUHealthComponent::OnTakeAmyDamage);
+	}
+}
+
+void USTUHealthComponent::OnTakeAmyDamage(
+    AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+    if (Damage <= 0.0f || IsDead() || !GetWorld()) return;
+	// clamp (значение, если меньше возвращает 0.0f, если больше то макс«доровье) две границы
+    SetHealth(Health - Damage);
+
+    GetWorld()->GetTimerManager().ClearTimer(HealTimer);
+
+	if (IsDead())
+	{
+        OnDeath.Broadcast();
+	}
+    else if (AutoHeal && GetWorld())
+    {
+        GetWorld()->GetTimerManager().SetTimer(HealTimer, this, &USTUHealthComponent::HealUpdate, HealUpdateTime, true, HealDelay);
+    }
+}
+
+void USTUHealthComponent::HealUpdate()
+{
+    SetHealth(Health + HealModifier);
+
+    if (FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld())
+    {
+        GetWorld()->GetTimerManager().ClearTimer(HealTimer);
+    }
+}
+
+void USTUHealthComponent::SetHealth(float NewHealth)
+{
+    Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
+    OnHealthChanged.Broadcast(Health);
+}
---
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -0,0 +1,59 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "STUCoreTypes.h"
+#include "STUHealthComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class SHOOTTHEMUP_API USTUHealthComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this component's properties
+    USTUHealthComponent();
+
+    FOnDeathSignature OnDeath;
+    FOnHealthChangedSignature OnHealthChanged;
+
+    UFUNCTION(BlueprintCallable, Category = "Health")
+    bool IsDead() const { return FMath::IsNearlyZero(Health); }
+
+    UFUNCTION(BlueprintCallable, Category = "Health")
+    float GetHealthPercent() const { return Health / MaxHealth; }
+
+    float GetHealth() const { return Health; }
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Health", meta = (ClampMin = "0.0", ClampMax = "1000.0"))
+    float MaxHealth = 100.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal")
+    bool AutoHeal = true;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal", meta = (EditCondition = "Autoheal"))
+    float HealUpdateTime = 0.5f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal", meta = (EditCondition = "Autoheal"))
+    float HealDelay = 3.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal", meta = (EditCondition = "Autoheal"))
+    float HealModifier = 1.0f;
+
+    // Called when the game starts
+    virtual void BeginPlay() override;
+
+private:
+    float Health = 0.0f;
+    FTimerHandle HealTimer;
+
+    UFUNCTION()
+    void OnTakeAmyDamage(
+        AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+    void HealUpdate();
+    void SetHealth(float NewHealth);
+};
