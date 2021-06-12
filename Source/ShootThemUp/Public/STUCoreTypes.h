8925fc18a13b67d402c487e57b70987ead222db0
Sergey Shpakov <guttleslug@mail.ru>
2021-06-13 01:50:59 +0300

    no message

--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -35,6 +35,8 @@ struct FWeaponData
     UAnimMontage* ReloadAnimMontage;
 };
 
+// UI
+
 USTRUCT(BlueprintType)
 struct FWeaponUIData
 {
--- Source/ShootThemUp/Public/UI/STUPlayerHUDWidget.h
+++ Source/ShootThemUp/Public/UI/STUPlayerHUDWidget.h
@@ -8,17 +8,26 @@
 #include "STUPlayerHUDWidget.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class SHOOTTHEMUP_API USTUPlayerHUDWidget : public UUserWidget
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
 public:
     UFUNCTION(BlueprintCallable, Category = "UI")
-	float GetHealthPercent() const;
+    float GetHealthPercent() const;
 
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	bool GetWeaponUIData(FWeaponUIData& UIData) const;
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool GetCurrentWeaponUIData(FWeaponUIData& UIData) const;
+
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool GetCurrentWeaponAmmoData(FAmmoData& AmmoData) const;
+
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool IsPlayerAlive() const;
+
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    bool IsPlayerSpectating() const;
 };
