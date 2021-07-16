1edcac90ca53b361b16696f6ba0f16b27e659ef6
Sergey Shpakov <guttleslug@mail.ru>
2021-07-16 04:48:34 +0300

    Сервисы и декораторы

---
+++ Source/ShootThemUp/Private/AI/Services/STU_FindEnemyService.cpp
@@ -0,0 +1,28 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "AI/Services/STU_FindEnemyService.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "STUUtils.h"
+#include "Components/STUAIPerceptionComponent.h"
+
+USTU_FindEnemyService::USTU_FindEnemyService() 
+{
+    NodeName = "Find Enemy";
+}
+
+void USTU_FindEnemyService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) 
+{
+    const auto Blackboard = OwnerComp.GetBlackboardComponent();
+    if (Blackboard)
+    {
+        const auto Controller = OwnerComp.GetAIOwner();
+        const auto PerceptionComponent = STUUtils::GetSTUPlayerComponent<USTUAIPerceptionComponent>(Controller);
+        if (PerceptionComponent)
+        {
+            Blackboard->SetValueAsObject(EnemyActorKey.SelectedKeyName, PerceptionComponent->GetClosestEnemy());
+        }
+    }
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
